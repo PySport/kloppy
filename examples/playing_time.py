5c994bdf9b23d3ed4a5ce0164c5643353a56369d
Koen Vossen <koen@teamtv.nl>
2020-05-15 23:01:29 +0200

    Add playing time example

---
+++ examples/playing_time.py
@@ -0,0 +1,31 @@
+from collections import Counter
+
+from kloppy import datasets
+import matplotlib.pyplot as plt
+
+
+def main():
+    """
+        This example shows how to determine playing time
+    """
+
+    data_set = datasets.load("metrica_tracking", options={'sample_rate': 1./25})
+
+    playing_seconds_per_player = Counter()
+    for frame in data_set.frames:
+        playing_seconds_per_player.update([int(jersey_no) for jersey_no in frame.home_team_player_positions.keys()])
+
+    x = range(len(playing_seconds_per_player))
+    jersey_numbers, playing_seconds = zip(*sorted(playing_seconds_per_player.items()))
+    playing_minutes = [seconds / 60 for seconds in playing_seconds]
+
+    plt.bar(x, playing_minutes, align='center', alpha=0.5)
+    plt.xticks(x, jersey_numbers)
+    plt.ylabel('Minutes')
+    plt.title('Playing time per player')
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()

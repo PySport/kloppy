52c3de5d669bad5de0557a6e4a85fb3ceafa4f68
FCrSTATS <jmu@fcn.dk>
2020-09-25 18:41:01 +0200

    Update pitch.py
    added distance_to_point function to the Pointt class

--- kloppy/domain/models/pitch.py
+++ kloppy/domain/models/pitch.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 
-
 @dataclass
 class Dimension:
     min: float
@@ -25,3 +24,7 @@ class PitchDimensions:
 class Point:
     x: float
     y: float
+
+    def distance_to_point(self, Point) -> int:
+        # returns the euclidean distance between the point and another provided point
+        return int(round(sum([(a - b)**2 for a, b in zip((self.x, self.y), (Point.x, Point.y))])**(1/2), 0))

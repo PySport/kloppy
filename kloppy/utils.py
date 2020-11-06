454f031c24d6121123d6607c25fa44f6c3442ff0
koenvo <info@koenvossen.nl>
2020-11-06 11:23:53 +0100

    Update utils.py

--- kloppy/utils.py
+++ kloppy/utils.py
@@ -47,4 +47,7 @@ def camelcase_to_snakecase(name):
 
 
 def removes_suffix(string, suffix):
-    return string[: -len(suffix)]
+    if string[-len(suffix):] == suffix:
+        return string[: -len(suffix)]
+    else:
+        return string

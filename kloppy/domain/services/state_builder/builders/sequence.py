24680dc7f2bb0f23a539b38c79b36d7a13844122
Bruno <bruno@metrica-sports.com>
2020-11-11 12:46:20 -0300

    Improve sequence definition based on latets added generic events
    In this commit, as proposed in #72 I implemented and improvement on the
    definition of sequences so that they are opened by a pass, carry or
    recovery, and closed by a ball out, fould committed or shot on target.
    
    This results also in sequences in which the team is None, whixh I think
    is interesting so you can look at events or tracking data when no team
    is in possession.

--- kloppy/domain/services/state_builder/builders/sequence.py
+++ kloppy/domain/services/state_builder/builders/sequence.py
@@ -1,7 +1,20 @@
 from dataclasses import replace, dataclass
 
-from kloppy.domain import Event, Team, EventDataset, PassEvent
+from kloppy.domain import (
+    Event,
+    Team,
+    EventDataset,
+    PassEvent,
+    CarryEvent,
+    RecoveryEvent,
+    BallOutEvent,
+    FoulCommittedEvent,
+    ShotEvent,
+)
 from ..builder import StateBuilder
+
+OPEN_SEQUENCE = (PassEvent, CarryEvent, RecoveryEvent)
+CLOSE_SEQUENCE = (BallOutEvent, FoulCommittedEvent, ShotEvent)
 
 
 @dataclass
@@ -11,15 +24,26 @@ class Sequence:
 
 
 class SequenceStateBuilder(StateBuilder):
+    def __init__(self):
+        self.sequence_open = False
+
     def initial_state(self, dataset: EventDataset) -> Sequence:
         for event in dataset.events:
-            if isinstance(event, PassEvent):
+            if isinstance(event, OPEN_SEQUENCE):
+                self.sequence_open = True
                 return Sequence(sequence_id=0, team=event.team)
         return Sequence(sequence_id=0, team=None)
 
     def reduce(self, state: Sequence, event: Event) -> Sequence:
-        if state.team != event.team:
+
+        if self.sequence_open and isinstance(event, CLOSE_SEQUENCE):
+            state = replace(
+                state, sequence_id=state.sequence_id + 1, team=None
+            )
+
+        if not self.sequence_open and isinstance(event, OPEN_SEQUENCE):
             state = replace(
                 state, sequence_id=state.sequence_id + 1, team=event.team
             )
+
         return state

23a7820f872bcb9f82e95dadc106e78d138a9808
Bruno <bruno@metrica-sports.com>
2020-11-16 15:06:19 -0300

    Improved state builder implementation: reduce_after and reduce_before
    In some cases a state needs to be updated before the event (like in
    recoveries, or passes) and in others after the event (like in score, or
    ball out).
    
    I have changed the single reduce instance which was taking place after
    the event, by `redice_before` and `reduce_after` methods which allow
    state builders to handle this two situations.

--- kloppy/domain/services/state_builder/builders/score.py
+++ kloppy/domain/services/state_builder/builders/score.py
@@ -17,7 +17,10 @@ class ScoreStateBuilder(StateBuilder):
     def initial_state(self, dataset: EventDataset) -> Score:
         return Score(home=0, away=0)
 
-    def reduce(self, state: Score, event: Event) -> Score:
+    def reduce_before(self, state: Score, event: Event) -> Score:
+        return state
+
+    def reduce_after(self, state: Score, event: Event) -> Score:
         if isinstance(event, ShotEvent):
             if event.result == ShotResult.GOAL:
                 if event.team.ground == Ground.HOME:
--- kloppy/domain/services/state_builder/builders/sequence.py
+++ kloppy/domain/services/state_builder/builders/sequence.py
@@ -24,26 +24,25 @@ class Sequence:
 
 
 class SequenceStateBuilder(StateBuilder):
-    def __init__(self):
-        self.sequence_open = False
-
     def initial_state(self, dataset: EventDataset) -> Sequence:
         for event in dataset.events:
             if isinstance(event, OPEN_SEQUENCE):
-                self.sequence_open = True
                 return Sequence(sequence_id=0, team=event.team)
         return Sequence(sequence_id=0, team=None)
 
-    def reduce(self, state: Sequence, event: Event) -> Sequence:
-
-        if self.sequence_open and isinstance(event, CLOSE_SEQUENCE):
-            state = replace(
-                state, sequence_id=state.sequence_id + 1, team=None
-            )
-
-        if not self.sequence_open and isinstance(event, OPEN_SEQUENCE):
+    def reduce_before(self, state: Sequence, event: Event) -> Sequence:
+        if isinstance(event, OPEN_SEQUENCE) and state.team != event.team:
             state = replace(
                 state, sequence_id=state.sequence_id + 1, team=event.team
+            )
+
+        return state
+
+    def reduce_after(self, state: Sequence, event: Event) -> Sequence:
+
+        if isinstance(event, CLOSE_SEQUENCE):
+            state = replace(
+                state, sequence_id=state.sequence_id + 1, team=None
             )
 
         return state

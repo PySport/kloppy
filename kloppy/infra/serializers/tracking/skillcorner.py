cb97980bf6fd6cbca8fdad35ef515663ecbc8c58
znstrider <mindfulstrider@googlemail.com>
2021-03-24 16:48:26 +0100

    add iter for sample_rate and limit plus logging

--- kloppy/infra/serializers/tracking/skillcorner.py
+++ kloppy/infra/serializers/tracking/skillcorner.py
@@ -1,3 +1,4 @@
+import logging
 from typing import List, Dict, Tuple
 from enum import Enum, Flag
 import numpy as np
@@ -26,8 +27,109 @@ from kloppy.utils import Readable, performance_logging
 
 from . import TrackingDataSerializer
 
+logger = logging.getLogger(__name__)
 
 class SkillCornerTrackingSerializer(TrackingDataSerializer):
+    @classmethod
+    def _get_frame_data(cls, teams, frame):
+        frame_id = frame["frame"]
+        frame_time = frame["time"]
+
+        ball_coordinates = None
+        players_coordinates = {}
+
+        ball_owning_player = frame["possession"].get(
+            "trackable_object"
+        )
+        ball_owning_team = frame["possession"].get("group")
+
+        if ball_owning_team == "home team":
+            ball_owning_team = teams[0]
+        elif ball_owning_team == "away team":
+            ball_owning_team = team[1]
+        else:
+            ball_owning_team = None
+
+        for frame_record in frame["data"]:
+            # containing x, y, trackable_object, track_id, group_name
+            x = frame_record.get("x")
+            y = frame_record.get("y")
+
+            trackable_object = frame_record.get(
+                "trackable_object", None
+            )
+            player_data = player_dict.get(trackable_object)
+
+            track_id = frame_record.get("track_id", None)
+            group_name = frame_record.get("group_name", None)
+
+            if trackable_object == ball_id:
+                group_name = "ball"
+                z = frame_record.get("z")
+                ball_coordinates = Point(x=float(x), y=float(y))
+                continue
+
+            elif trackable_object in referee_dict.keys():
+                group_name = "referee"
+                continue  # Skip Referee Coords
+
+            if group_name is None:
+                group_name = teamdict.get(
+                    player_id_to_team_dict.get(trackable_object)
+                )
+
+                if group_name == "home_team":
+                    player = players["HOME"][trackable_object]
+                elif group_name == "away_team":
+                    player = players["AWAY"][trackable_object]
+
+            if trackable_object is None:
+                player_id = str(track_id)
+                if group_name == "home team":
+                    if (
+                        f"anon_{player_id}"
+                        not in anon_players["HOME"].keys()
+                    ):
+                        player = self.__create_anon_player(
+                            frame_record
+                        )
+                        anon_players["HOME"][
+                            f"anon_home_{player_id}"
+                        ] = player
+                    else:
+                        player = anon_players["HOME"][
+                            f"anon_home_{player_id}"
+                        ]
+
+                elif group_name == "away team":
+                    if (
+                        f"anon_{player_id}"
+                        not in anon_players["AWAY"].keys()
+                    ):
+                        player = self.__create_anon_player(
+                            frame_record
+                        )
+                        anon_players["AWAY"][
+                            f"anon_away_{player_id}"
+                        ] = player
+                    else:
+                        player = anon_players["AWAY"][
+                            f"anon_away_{player_id}"
+                        ]
+
+            point = Point(x, y)
+            players_coordinates[player] = point
+
+        return Frame(
+            frame_id=frame_id,
+            timestamp=frame_time,
+            ball_coordinates=ball_coordinates,
+            players_coordinates=players_coordinates,
+            period=periods[frame_period],
+            ball_state=None,
+            ball_owning_team=ball_owning_team,
+        )
+
     def __load_json(self, file):
         return json.load(file)
 
@@ -148,193 +250,118 @@ class SkillCornerTrackingSerializer(TrackingDataSerializer):
 
         sample_rate = float(options.get("sample_rate", 1.0))
         limit = int(options.get("limit", 0))
-        only_alive = bool(options.get("only_alive", True))
+        # only_alive = bool(options.get("only_alive", True))
 
-        periods = self.__get_periods(raw_data)
+        with performance_logging("Loading metadata", logger=logger):
+            periods = self.__get_periods(raw_data)
 
-        teamdict = {
-            metadata["home_team"].get("id"): "home_team",
-            metadata["away_team"].get("id"): "away_team",
-        }
+            teamdict = {
+                metadata["home_team"].get("id"): "home_team",
+                metadata["away_team"].get("id"): "away_team",
+            }
 
-        player_id_to_team_dict = {
-            player["trackable_object"]: player["team_id"]
-            for player in metadata["players"]
-        }
+            player_id_to_team_dict = {
+                player["trackable_object"]: player["team_id"]
+                for player in metadata["players"]
+            }
 
-        player_dict = {
-            player["trackable_object"]: player
-            for player in metadata["players"]
-        }
+            player_dict = {
+                player["trackable_object"]: player
+                for player in metadata["players"]
+            }
 
-        referee_dict = {
-            ref["trackable_object"]: "referee" for ref in metadata["referees"]
-        }
-        ball_id = metadata["ball"]["trackable_object"]
+            referee_dict = {
+                ref["trackable_object"]: "referee" for ref in metadata["referees"]
+            }
+            ball_id = metadata["ball"]["trackable_object"]
 
-        # there are different pitch_sizes in SkillCorner
-        pitch_size_width = metadata["pitch_width"]
-        pitch_size_length = metadata["pitch_length"]
+            # there are different pitch_sizes in SkillCorner
+            pitch_size_width = metadata["pitch_width"]
+            pitch_size_length = metadata["pitch_length"]
 
-        home_team_id = metadata["home_team"]["id"]
-        away_team_id = metadata["away_team"]["id"]
+            home_team_id = metadata["home_team"]["id"]
+            away_team_id = metadata["away_team"]["id"]
 
-        players = {"HOME": {}, "AWAY": {}}
+            players = {"HOME": {}, "AWAY": {}}
 
-        home_team = Team(
-            team_id=home_team_id,
-            name=metadata["home_team"]["name"],
-            ground=Ground.HOME,
-        )
-        self.home_team = home_team
-
-        away_team = Team(
-            team_id=away_team_id,
-            name=metadata["away_team"]["name"],
-            ground=Ground.AWAY,
-        )
-        self.away_team = away_team
-
-        teams = [home_team, away_team]
-
-        for player_id in player_dict.keys():
-            player = player_dict.get(player_id)
-            team_id = player["team_id"]
-
-            if team_id == home_team_id:
-                team_string = "HOME"
-                team = home_team
-            elif team_id == away_team_id:
-                team_string = "AWAY"
-                team = away_team
-
-            players[team_string][player_id] = Player(
-                player_id=f"{team.ground}_{player['number']}",
-                team=team,
-                jersey_no=player["number"],
-                name=" ".join([player["first_name"], player["last_name"]]),
-                first_name=player["first_name"],
-                last_name=player["last_name"],
-                starting=player["start_time"] == "00:00:00",
-                position=Position(
-                    position_id=player["player_role"].get("id"),
-                    name=player["player_role"].get("name"),
-                    coordinates=None,
-                ),
-                attributes={},
+            home_team = Team(
+                team_id=home_team_id,
+                name=metadata["home_team"]["name"],
+                ground=Ground.HOME,
             )
+            self.home_team = home_team
 
-        home_team.players = list(players["HOME"].values())
-        away_team.players = list(players["AWAY"].values())
+            away_team = Team(
+                team_id=away_team_id,
+                name=metadata["away_team"]["name"],
+                ground=Ground.AWAY,
+            )
+            self.away_team = away_team
+
+            teams = [home_team, away_team]
+
+            for player_id in player_dict.keys():
+                player = player_dict.get(player_id)
+                team_id = player["team_id"]
+
+                if team_id == home_team_id:
+                    team_string = "HOME"
+                    team = home_team
+                elif team_id == away_team_id:
+                    team_string = "AWAY"
+                    team = away_team
+
+                players[team_string][player_id] = Player(
+                    player_id=f"{team.ground}_{player['number']}",
+                    team=team,
+                    jersey_no=player["number"],
+                    name=" ".join([player["first_name"], player["last_name"]]),
+                    first_name=player["first_name"],
+                    last_name=player["last_name"],
+                    starting=player["start_time"] == "00:00:00",
+                    position=Position(
+                        position_id=player["player_role"].get("id"),
+                        name=player["player_role"].get("name"),
+                        coordinates=None,
+                    ),
+                    attributes={},
+                )
+
+            home_team.players = list(players["HOME"].values())
+            away_team.players = list(players["AWAY"].values())
 
         anon_players = {"HOME": {}, "AWAY": {}}
 
+        with performance_logging("Loading data", logger=logger):
+
+            def _iter():
+                n=0
+                sample = 1.0 / sample_rate
+
+                for frame in raw_data:
+                    frame_period = frame["period"]
+
+                    if frame_period is not None:
+                        if n % sample == 0:
+                            yield frame
+                        n += 1
+
         frames = []
+        for n, _frame in enumerate(_iter()):
+            frame = self._get_frame_data(teams, _frame)
 
-        for frame in raw_data:
-            frame_period = frame["period"]
+            frames.append(frame)
 
-            # before the actual start of a period there can be empty list entries
-            if frame_period is not None:
-                frame_id = frame["frame"]
-                frame_time = frame["time"]
+            # TODO: Set Attacking Direction 
+            # if not period.attacking_direction_set:
+            #    period.set_attacking_direction(
+            #        attacking_direction=attacking_direction_from_frame(
+            #            frame
+            #        )
+            #    )
 
-                ball_coordinates = None
-                players_coordinates = {}
-
-                ball_owning_player = frame["possession"].get(
-                    "trackable_object"
-                )
-                ball_owning_team = frame["possession"].get("group")
-
-                if ball_owning_team == "home team":
-                    ball_owning_team = home_team
-                elif ball_owning_team == "away team":
-                    ball_owning_team = away_team
-                else:
-                    ball_owning_team = None
-
-                for frame_record in frame["data"]:
-                    # containing x, y, trackable_object, track_id, group_name
-                    x = frame_record.get("x")
-                    y = frame_record.get("y")
-
-                    trackable_object = frame_record.get(
-                        "trackable_object", None
-                    )
-                    player_data = player_dict.get(trackable_object)
-
-                    track_id = frame_record.get("track_id", None)
-                    group_name = frame_record.get("group_name", None)
-
-                    if trackable_object == ball_id:
-                        group_name = "ball"
-                        z = frame_record.get("z")
-                        ball_coordinates = Point(x=float(x), y=float(y))
-                        continue
-
-                    elif trackable_object in referee_dict.keys():
-                        group_name = "referee"
-                        continue  # Skip Referee Coords
-
-                    if group_name is None:
-                        group_name = teamdict.get(
-                            player_id_to_team_dict.get(trackable_object)
-                        )
-
-                        if group_name == "home_team":
-                            player = players["HOME"][trackable_object]
-                        elif group_name == "away_team":
-                            player = players["AWAY"][trackable_object]
-
-                    if trackable_object is None:
-                        player_id = str(track_id)
-                        if group_name == "home team":
-                            if (
-                                f"anon_{player_id}"
-                                not in anon_players["HOME"].keys()
-                            ):
-                                player = self.__create_anon_player(
-                                    frame_record
-                                )
-                                anon_players["HOME"][
-                                    f"anon_home_{player_id}"
-                                ] = player
-                            else:
-                                player = anon_players["HOME"][
-                                    f"anon_home_{player_id}"
-                                ]
-
-                        elif group_name == "away team":
-                            if (
-                                f"anon_{player_id}"
-                                not in anon_players["AWAY"].keys()
-                            ):
-                                player = self.__create_anon_player(
-                                    frame_record
-                                )
-                                anon_players["AWAY"][
-                                    f"anon_away_{player_id}"
-                                ] = player
-                            else:
-                                player = anon_players["AWAY"][
-                                    f"anon_away_{player_id}"
-                                ]
-
-                    point = Point(x, y)
-                    players_coordinates[player] = point
-
-                frame = Frame(
-                    frame_id=frame_id,
-                    timestamp=frame_time,
-                    ball_coordinates=ball_coordinates,
-                    players_coordinates=players_coordinates,
-                    period=periods[frame_period],
-                    ball_state=None,
-                    ball_owning_team=ball_owning_team,
-                )
-
-                frames.append(frame)
+            if limit and n >= limit:
+                break
 
         frame_rate = 1
 

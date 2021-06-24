8d01e87612f7e11dea3d0f56c4d014846885b8a3
Bruno <bruno@metrica-sports.com>
2021-06-24 15:30:40 -0300

    Modify etps serializer so that it accepts files with no ball z
    I also added files and test to test this. As this is how Metrica Sports
    provides the data (no z coordinate) I included this files and test as
    testing the serialization of Metrica Sports in particular, which I
    believe is something good to have (test for each provider, not just for
    ETPS in general)

--- kloppy/tests/test_epts.py
+++ kloppy/tests/test_epts.py
@@ -47,7 +47,9 @@ class TestEPTSTracking:
 
     def test_provider_name_recognition(self):
         base_dir = os.path.dirname(__file__)
-        with open(f"{base_dir}/files/epts_metrica.xml", "rb") as metadata_fp:
+        with open(
+            f"{base_dir}/files/epts_metrica_metadata.xml", "rb"
+        ) as metadata_fp:
             root = objectify.fromstring(metadata_fp.read())
             metadata = root.find("Metadata")
             provider_from_file = _load_provider(metadata)
@@ -125,4 +127,33 @@ class TestEPTSTracking:
 
         assert dataset.records[0].ball_coordinates == Point3D(
             x=-2656, y=367, z=100
+        )
+
+    def test_deserialize_metrica_epts(self):
+        base_dir = os.path.dirname(__file__)
+
+        serializer = EPTSSerializer()
+
+        with open(
+            f"{base_dir}/files/epts_metrica_metadata.xml", "rb"
+        ) as metadata, open(
+            f"{base_dir}/files/epts_metrica_tracking.txt", "rb"
+        ) as raw_data:
+
+            dataset = serializer.deserialize(
+                inputs={"metadata": metadata, "raw_data": raw_data}
+            )
+
+        first_player = next(iter(dataset.records[0].players_coordinates))
+
+        assert len(dataset.records) == 11
+        assert len(dataset.metadata.periods) == 2
+        assert dataset.metadata.orientation is None
+
+        assert dataset.records[0].players_coordinates[first_player] == Point(
+            x=0.85708, y=0.50652
+        )
+
+        assert dataset.records[0].ball_coordinates == Point(
+            x=0.54711, y=0.53978
         )

970a9920673e005e0260b0ac13d0479ed89a7120
Koen Vossen <info@koenvossen.nl>
2022-03-18 22:42:05 +0100

    Code formatting

--- kloppy/infra/io/adapters/__init__.py
+++ kloppy/infra/io/adapters/__init__.py
@@ -4,10 +4,7 @@ from .adapter import Adapter
 from .http import HTTPAdapter
 from .s3 import S3Adapter
 
-adapters = {
-    "http": HTTPAdapter(),
-    "s3": S3Adapter()
-}
+adapters = {"http": HTTPAdapter(), "s3": S3Adapter()}
 
 
 def get_adapter(url: str) -> Optional[Adapter]:

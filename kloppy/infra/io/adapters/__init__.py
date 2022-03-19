6fe875a12f08ae1b04a0b13932f67de2842a03cc
Koen Vossen <info@koenvossen.nl>
2022-03-19 11:26:12 +0100

    Lists are easy enough to replace items in, and it otherwise it will look like the key of the dictionary has any meaning (like the scheme it supports) - which is not

--- kloppy/infra/io/adapters/__init__.py
+++ kloppy/infra/io/adapters/__init__.py
@@ -4,10 +4,10 @@ from .adapter import Adapter
 from .http import HTTPAdapter
 from .s3 import S3Adapter
 
-adapters = {"http": HTTPAdapter(), "s3": S3Adapter()}
+adapters = [HTTPAdapter(), S3Adapter()]
 
 
 def get_adapter(url: str) -> Optional[Adapter]:
-    for adapter in adapters.values():
+    for adapter in adapters:
         if adapter.supports(url):
             return adapter
--- kloppy/infra/io/adapters/s3.py
+++ kloppy/infra/io/adapters/s3.py
@@ -7,10 +7,13 @@ from .adapter import Adapter
 
 
 class S3Adapter(Adapter):
+    def __init__(self):
+        self.s3_fs = None
+
     def supports(self, url: str) -> bool:
         return url.startswith("s3://")
 
-    def read_to_stream(self, url: str, output: BinaryIO):
+    def _get_s3fs(self):
         try:
             import s3fs
         except ImportError:
@@ -21,7 +24,14 @@ class S3Adapter(Adapter):
 
         s3_fs = get_config("io.adapters.s3.s3fs")
         if not s3_fs:
-            s3_fs = s3fs.S3FileSystem()
+            if not self.s3_fs:
+                s3_fs = s3fs.S3FileSystem()
+            else:
+                s3_fs = self.s3_fs
+        return s3_fs
+
+    def read_to_stream(self, url: str, output: BinaryIO):
+        s3_fs = self._get_s3fs()
 
         with s3_fs.open(url, "rb") as fp:
             shutil.copyfile(fp, output)

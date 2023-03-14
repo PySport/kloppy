2df5b673abd3d08fb5b66d6764f67f06c5fa68e9
driesdeprest <driesdeprest1@gmail.com>
2023-03-14 17:55:48 +0100

    Remove Python 3.10 syntax of typing

--- kloppy/_providers/wyscout.py
+++ kloppy/_providers/wyscout.py
@@ -63,7 +63,7 @@ def load_open_data(
 
 def identify_deserializer(
     event_data: FileLike,
-) -> Type[WyscoutDeserializerV3 | WyscoutDeserializerV2]:
+) -> Union[Type[WyscoutDeserializerV3], Type[WyscoutDeserializerV2]]:
     with open_as_file(event_data) as event_data_fp:
         events_with_meta = json.load(event_data_fp)
 

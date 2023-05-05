5deb6beec02ab4cde9a91030454ed5465d9a7c3f
Rishi Aluri <rishialuri@gmail.com>
2023-05-05 12:10:07 +0100

    Issue#60: Removing deleted events from OPTA f24

--- kloppy/tests/issues/issue_60/test_issue_60.py
+++ kloppy/tests/issues/issue_60/test_issue_60.py
@@ -1,45 +1,22 @@
-import os
-
-from kloppy.domain import Orientation
 from kloppy import opta
-
-
-def kloppy_load_data(f7, f24):
-    """
-
-    Args:
-        f7: filepath to the match details
-        f24: filepath to the event details with deleted event
-
-
-    Returns:
-        events: DataFrame consisting of all events
-        home_team_id: id of the home team
-        away_team_id: id of the away team
-
-    """
-    dataset = opta.load(f7_data=f7, f24_data=f24)
-
-    events = dataset.transform(
-        to_orientation=Orientation.FIXED_HOME_AWAY
-    ).to_df(
-        additional_columns={
-            "event_name": lambda event: str(getattr(event, "event_name", "")),
-            "player_name": lambda event: str(getattr(event, "player", "")),
-            "ball_state": lambda event: str(getattr(event, "ball_state", "")),
-            "team_name": lambda event: str(getattr(event, "team", "")),
-        },
-    )
-
-    metadata = dataset.metadata
-    home_team, away_team = metadata.teams
-    return events, home_team.team_id, away_team.team_id
+import os
 
 
 class TestIssue60:
-    def test_parse_opta(self):
+    def test_deleted_event_opta(self):
         dir_path = os.path.dirname(__file__)
-        kloppy_load_data(
-            f7=f"{dir_path}/opta_f7.xml",
-            f24=f"{dir_path}/opta_f24.xml",
+        deleted_event_id = "2087733359"
+
+        event_dataset = opta.load(
+            f7_data=os.path.join(dir_path, "opta_f7.xml"),
+            f24_data=os.path.join(dir_path, "opta_f24.xml"),
         )
+        df = event_dataset.to_df()
+
+        assert deleted_event_id not in df["event_id"].to_list()
+
+        # OPTA F24 file: Pass -> Deleted Event -> Tackle
+        assert event_dataset.events[14].event_name == "pass"
+        assert (
+            event_dataset.events[15].event_name == "tackle"
+        )  # Deleted Event is filter out

b8802eceb2281254d4b11dfd342e282a36c5b91e
Koen Vossen <info@koenvossen.nl>
2023-07-21 22:27:07 +0200

    Refactor Sportec.event deserializer to read all metadata in a separate method. This way the metadata parsing for Sportec can be shared between the event- and trackingdata Deserializers

--- kloppy/_providers/sportec.py
+++ kloppy/_providers/sportec.py
@@ -3,13 +3,14 @@ from typing import Optional, List
 from kloppy.config import get_config
 from kloppy.domain import EventDataset, EventFactory
 from kloppy.infra.serializers.event.sportec import (
-    SportecEventDeserializer,
-    SportecInputs,
+    SportecEventDataDeserializer,
+    SportecEvenDataInputs,
 )
 from kloppy.io import open_as_file
+from kloppy.utils import deprecated
 
 
-def load(
+def load_event(
     event_data: str,
     meta_data: str,
     event_types: Optional[List[str]] = None,
@@ -27,7 +28,7 @@ def load(
         event_factory:
 
     """
-    serializer = SportecEventDeserializer(
+    serializer = SportecEventDataDeserializer(
         event_types=event_types,
         coordinate_system=coordinates,
         event_factory=event_factory or get_config("event_factory"),
@@ -36,5 +37,20 @@ def load(
         meta_data
     ) as meta_data_fp:
         return serializer.deserialize(
-            SportecInputs(event_data=event_data_fp, meta_data=meta_data_fp)
+            SportecEvenDataInputs(
+                event_data=event_data_fp, meta_data=meta_data_fp
+            )
         )
+
+
+@deprecated("sportec.load_event should be used")
+def load(
+    event_data: str,
+    meta_data: str,
+    event_types: Optional[List[str]] = None,
+    coordinates: Optional[str] = None,
+    event_factory: Optional[EventFactory] = None,
+) -> EventDataset:
+    return load_event(
+        event_data, meta_data, event_types, coordinates, event_factory
+    )

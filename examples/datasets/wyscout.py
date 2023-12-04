767ff06cb3bd782d12357f278c825387c055895c
Koen Vossen <info@koenvossen.nl>
2023-12-04 09:55:43 +0100

    Minor

--- examples/datasets/wyscout.py
+++ examples/datasets/wyscout.py
@@ -7,7 +7,7 @@ from kloppy.utils import performance_logging
 
 def main():
     """
-    This example shows the use of Metrica datasets, and how we can pass argument
+    This example shows the use of Wyscout datasets, and how we can pass argument
     to the dataset loader.
     """
     logging.basicConfig(
@@ -16,7 +16,7 @@ def main():
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     )
 
-    # The metrica dataset loader loads by default the 'game1' dataset
+    # The wyscout dataset loader loads by default the '2499841' dataset
     dataset = wyscout.load_open_data()
     print(len(dataset.events))
 

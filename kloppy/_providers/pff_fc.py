b26c989a2dd2c4073c66ee6854771c0b11352be6
razor3598 <amdsah42@gmail.com>
2024-12-06 09:42:09 -0500

    Fixed roster_meta_data

--- kloppy/_providers/pff_fc.py
+++ kloppy/_providers/pff_fc.py
@@ -7,7 +7,7 @@ from kloppy.io import open_as_file, FileLike
 
 def load_tracking(
     meta_data: FileLike,
-    players_meta_data: FileLike,
+    roster_meta_data: FileLike,
     raw_data: FileLike,
     sample_rate: Optional[float] = None,
     limit: Optional[int] = None,
@@ -20,9 +20,9 @@ def load_tracking(
         coordinate_system=coordinates,
         include_empty_frames=include_empty_frames,
     )
-    with open_as_file(meta_data) as meta_data_fp, open_as_file(players_meta_data) as players_meta_data_fp:
+    with open_as_file(meta_data) as meta_data_fp, open_as_file(roster_meta_data) as roster_meta_data_fp:
         return deserializer.deserialize(
             inputs=PFF_FCTrackingInputs(
-                meta_data=meta_data_fp, players_meta_data=players_meta_data_fp, raw_data=raw_data
+                meta_data=meta_data_fp, roster_meta_data=roster_meta_data_fp, raw_data=raw_data
             )
         )

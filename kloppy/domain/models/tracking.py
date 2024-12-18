79bb6d957530666d73796c3b80e49bd4231d385a
Pieter Robberechts <pieter.robberechts@kuleuven.be>
2024-12-18 13:17:40 +0100

    Remove speed related code

--- kloppy/domain/models/tracking.py
+++ kloppy/domain/models/tracking.py
@@ -1,6 +1,5 @@
-from collections import defaultdict
 from dataclasses import dataclass, field, replace
-from math import ceil, isnan
+from math import isnan
 from typing import Any, Callable, Dict, List, Optional, Union
 
 try:
@@ -9,24 +8,10 @@ except ImportError:
     from typing_extensions import Literal
 
 from kloppy.domain.models.common import DatasetType
-from kloppy.domain.models.pitch import (
-    DEFAULT_PITCH_LENGTH,
-    DEFAULT_PITCH_WIDTH,
-)
-from kloppy.exceptions import KloppyError
 from kloppy.utils import deprecated
 
 from .common import DataRecord, Dataset, Player
 from .pitch import Point
-
-try:
-    import numpy as np
-except ImportError:
-    np = None
-try:
-    from scipy.signal import savgol_filter
-except ImportError:
-    savgol_filter = None
 
 
 @dataclass
@@ -112,6 +97,23 @@ class Frame(DataRecord):
             for player, player_data in self.players_data.items()
         }
 
+    def __getitem__(self, object_id: Union[str, Player]):
+        """Get the data for a specific object in the frame.
+
+        Args:
+            object_id: The ID of the object to get data for. Either a `Player`, player ID or "ball".
+        Returns:
+            The data for the object or `None` if the object is not in the frame.
+        """
+        if object_id == "ball":
+            return self.ball_data
+        if isinstance(object_id, Player):
+            return self.players_data.get(object_id, None)
+        for player in self.players_data.keys():
+            if player.player_id == object_id:
+                return self.players_data[player]
+        return None
+
 
 @dataclass
 class TrackingDataset(Dataset[Frame]):
@@ -143,11 +145,12 @@ class TrackingDataset(Dataset[Frame]):
         trajectories = []
         current_trajectory = None
         for frame in self.records:
+            detection = frame[trackable_object]
             if (
-                trackable_object in frame.objects
-                and frame.objects[trackable_object].coordinates is not None
-                and not isnan(frame.objects[trackable_object].coordinates.x)
-                and not isnan(frame.objects[trackable_object].coordinates.y)
+                detection is not None
+                and detection.coordinates is not None
+                and not isnan(detection.coordinates.x)
+                and not isnan(detection.coordinates.y)
             ):
                 # The object is tracked in this frame
                 if current_trajectory is None:
@@ -157,7 +160,7 @@ class TrackingDataset(Dataset[Frame]):
                         trackable_object=trackable_object,
                         start_frame=frame.frame_id,
                         end_frame=frame.frame_id,
-                        detections=[frame.objects[trackable_object]],
+                        detections=[detection],
                     )
                 elif (
                     frame.prev_record is not None
@@ -168,9 +171,7 @@ class TrackingDataset(Dataset[Frame]):
                     # and it was tracked in the previous frame --> extend the
                     # current trajectory
                     current_trajectory.end_frame = frame.frame_id
-                    current_trajectory.detections.append(
-                        frame.objects[trackable_object]
-                    )
+                    current_trajectory.detections.append(detection)
                 else:
                     # but a frame is missing or a new period started --> finish
                     # the current trajectory and start a new one
@@ -179,7 +180,7 @@ class TrackingDataset(Dataset[Frame]):
                         trackable_object=trackable_object,
                         start_frame=frame.frame_id,
                         end_frame=frame.frame_id,
-                        detections=[frame.objects[trackable_object]],
+                        detections=[detection],
                     )
             else:
                 # The object is not tracked in this frame --> finish the
@@ -191,90 +192,6 @@ class TrackingDataset(Dataset[Frame]):
         if current_trajectory:
             trajectories.append(current_trajectory)
         return trajectories
-
-    def compute_ball_kinematics(
-        self,
-        n_smooth_speed: int = 6,
-        n_smooth_acc: int = 10,
-        filter_type: Optional[
-            Literal["savitzky_golay", "moving_average"]
-        ] = "savitzky_golay",
-        polyorder: int = 3,
-        window_length: int = 31,
-        max_speed: float = 50.0,
-        copy: bool = True,
-    ):
-        """Compute speed and acceleration of the ball.
-
-        Args:
-            n_smooth_speed: The number of frames to smooth over when computing speed.
-            n_smooth_acc: The number of frames to smooth over when computing acceleration.
-            filter_type: The type of filter to use when smoothing. Either "savitzky_golay", "moving_average" or None.
-            polyorder: Polyorder to use when the savitzky_golay filter is selected. Defaults to 3.
-            window_length: Window length of the filter. Defaults to 31 frames.
-            max_speed_player: The maximum speed allowed for a player (in m/s).
-            max_speed_ball: The maximum speed allowed for the ball (in m/s).
-            copy: If True, a new dataset is returned with the computed kinematics. If False, the dataset is modified in place.
-        Returns:
-            TrackingDataset: A new dataset with the computed kinematics.
-
-        See Also:
-            compute_player_kinematics
-        """
-        new_dataset = self._copy_detections() if copy else self
-        new_dataset._add_kinematics(
-            trackable_object="ball",
-            n_smooth_speed=n_smooth_speed,
-            n_smooth_acc=n_smooth_acc,
-            filter_type=filter_type,
-            polyorder=polyorder,
-            window_length=window_length,
-            max_speed=max_speed,
-        )
-        return new_dataset
-
-    def compute_player_kinematics(
-        self,
-        n_smooth_speed: int = 6,
-        n_smooth_acc: int = 10,
-        filter_type: Optional[
-            Literal["savitzky_golay", "moving_average"]
-        ] = "savitzky_golay",
-        polyorder: int = 3,
-        window_length: int = 31,
-        max_speed: float = 12.0,
-        copy: bool = True,
-    ):
-        """Compute speed and acceleration for each player in the dataset.
-
-        Args:
-            n_smooth_speed: The number of frames to smooth over when computing speed.
-            n_smooth_acc: The number of frames to smooth over when computing acceleration.
-            filter_type: The type of filter to use when smoothing. Either "savitzky_golay", "moving_average" or None.
-            polyorder: Polyorder to use when the savitzky_golay filter is selected. Defaults to 3.
-            window_length: Window length of the filter. Defaults to 31 frames.
-            max_speed_player: The maximum speed allowed for a player (in m/s).
-            max_speed_ball: The maximum speed allowed for the ball (in m/s).
-            copy: If True, a new dataset is returned with the computed kinematics. If False, the dataset is modified in place.
-        Returns:
-            TrackingDataset: A new dataset with the computed kinematics.
-
-        See Also:
-            compute_ball_kinematics
-        """
-        new_dataset = self._copy_detections() if copy else self
-        for team in self.metadata.teams:
-            for player in team.players:
-                new_dataset._add_kinematics(
-                    trackable_object=player,
-                    n_smooth_speed=n_smooth_speed,
-                    n_smooth_acc=n_smooth_acc,
-                    filter_type=filter_type,
-                    polyorder=polyorder,
-                    window_length=window_length,
-                    max_speed=max_speed,
-                )
-        return new_dataset
 
     def _copy_detections(self):
         """Create a copy of each detection in the tracking dataset."""
@@ -291,148 +208,6 @@ class TrackingDataset(Dataset[Frame]):
                 for record in self.records
             ],
         )
-
-    def _add_kinematics(
-        self,
-        trackable_object: Union[Player, Literal["ball"]],
-        n_smooth_speed: int,
-        n_smooth_acc: int,
-        filter_type: Optional[Literal["savitzky_golay", "moving_average"]],
-        polyorder: int,
-        window_length: int,
-        max_speed: float,
-    ) -> None:
-        """Add speed and acceleration to each each detection of an object in the dataset.
-
-        Args:
-            trackable_object: The object to compute kinematics for. Either a player or "ball".
-            n_smooth_speed: The number of frames to smooth over when computing speed.
-            n_smooth_acc: The number of frames to smooth over when computing acceleration.
-            filter_type: The type of filter to use when smoothing. Either "savitzky_golay", "moving_average" or None.
-            polyorder: Polyorder to use when the savitzky_golay filter is selected. Defaults to 3.
-            window_length: Window length of the filter. Defaults to 31 frames.
-            max_speed_player: The maximum speed allowed for a player (in m/s).
-            max_speed_ball: The maximum speed allowed for the ball (in m/s).
-        """
-        if np is None:
-            raise ImportError(
-                "Numpy is required to compute kinematics. Please install it using: pip install numpy"
-            )
-
-        if self.metadata.frame_rate is None:
-            raise KloppyError(
-                "Frame rate is not set in metadata. Please set the frame rate before computing kinematics."
-            )
-
-        n_smooth_speed = 1 if n_smooth_speed < 1 else n_smooth_speed
-        n_smooth_acc = 1 if n_smooth_acc < 1 else n_smooth_acc
-
-        for trajectory in self.trajectories(trackable_object):
-            if len(trajectory) < n_smooth_speed + 1:
-                for i, detection in enumerate(trajectory):
-                    detection.distance = np.nan
-                    detection.speed = np.nan
-                    detection.acceleration = np.nan
-                continue
-
-            # get x-y coordinates in metric space
-            tracked_maps = np.empty((len(trajectory), 2))
-            for i, detection in enumerate(trajectory):
-                point = detection.coordinates
-                metric_point = self.metadata.pitch_dimensions.to_metric_base(
-                    point,
-                    pitch_length=self.metadata.pitch_dimensions.pitch_length
-                    or DEFAULT_PITCH_LENGTH,
-                    pitch_width=self.metadata.pitch_dimensions.pitch_width
-                    or DEFAULT_PITCH_WIDTH,
-                )
-                tracked_maps[i] = [metric_point.x, metric_point.y]
-
-            # apply a filter for smoothing
-            if filter_type == "savitzky_golay":
-                tracked_maps = _apply_savgol_filter(
-                    tracked_maps, polyorder, window_length
-                )
-            elif filter_type == "moving_average":
-                tracked_maps = _apply_savgol_filter(
-                    tracked_maps, 0, window_length
-                )
-            elif filter_type is not None:
-                raise ValueError(
-                    f"Unknown filter type: {filter_type}. Supported types are 'savitzky_golay' and 'moving_average'."
-                )
-
-            # get speed vect and speed norm
-            dist = (
-                tracked_maps[n_smooth_speed:] - tracked_maps[:-n_smooth_speed]
-            )
-            dist_norm = np.linalg.norm(dist, axis=1) / n_smooth_speed
-
-            speed_vect = dist / (n_smooth_speed / self.metadata.frame_rate)
-            speed_norm = np.linalg.norm(speed_vect, axis=1)
-
-            # acc process for short tracks
-            if speed_vect.shape[0] < self.metadata.frame_rate:
-                acc_vect = np.nan * np.ones_like(speed_vect)
-                acc_norm = np.nan * np.ones_like(speed_norm)
-            else:
-                # acc vect process for other tracks
-                diff_acc = (
-                    speed_vect[n_smooth_acc:] - speed_vect[:-n_smooth_acc]
-                )
-                acc_vect = diff_acc / (n_smooth_acc / self.metadata.frame_rate)
-
-                # padding to respect the shape after the smoothing
-                add_acc_before = np.zeros((ceil(n_smooth_acc / 2), 2)) + np.nan
-                add_acc_after = np.zeros((n_smooth_acc // 2, 2)) + np.nan
-                acc_vect = np.concatenate(
-                    (add_acc_before, acc_vect, add_acc_after)
-                )
-
-                # apply a physical check based on speed and acc
-                acc_vect = _apply_criterion(speed_vect, acc_vect, max_speed)
-
-                # acc norm process for other tracks
-                diff_acc = (
-                    speed_norm[n_smooth_acc:] - speed_norm[:-n_smooth_acc]
-                )
-                acc_norm = diff_acc / (n_smooth_acc / self.metadata.frame_rate)
-
-                # padding to respect the shape after the smoothing
-                add_acc_norm_before = (
-                    np.zeros((ceil(n_smooth_acc / 2))) + np.nan
-                )
-                add_acc_norm_after = np.zeros((n_smooth_acc // 2)) + np.nan
-                acc_norm = np.concatenate(
-                    (add_acc_norm_before, acc_norm, add_acc_norm_after)
-                )
-
-            # apply last padding
-            add_speed_before = np.zeros((ceil(n_smooth_speed / 2), 2)) + np.nan
-            add_speed_after = np.zeros((n_smooth_speed // 2, 2)) + np.nan
-            speed_vect = np.concatenate(
-                (add_speed_before, speed_vect, add_speed_after)
-            )
-
-            add_speed_norm_before = (
-                np.zeros((ceil(n_smooth_speed / 2))) + np.nan
-            )
-            add_speed_norm_after = np.zeros((n_smooth_speed // 2)) + np.nan
-            dist_norm = np.concatenate(
-                (add_speed_norm_before, dist_norm, add_speed_norm_after)
-            )
-            speed_norm = np.concatenate(
-                (add_speed_norm_before, speed_norm, add_speed_norm_after)
-            )
-            acc_norm = np.concatenate(
-                (add_speed_norm_before, acc_norm, add_speed_norm_after)
-            )
-
-            # fill detection dict with physical info
-            for i, detection in enumerate(trajectory):
-                detection.distance = dist_norm[i]
-                detection.speed = speed_norm[i]
-                detection.acceleration = acc_norm[i]
 
     @deprecated(
         "to_pandas will be removed in the future. Please use to_df instead."
@@ -474,49 +249,6 @@ class TrackingDataset(Dataset[Frame]):
         return pd.DataFrame.from_records(
             map(generic_record_converter, self.records)
         )
-
-
-def _apply_criterion(speeds, acc, max_speed):
-    """Criterion used to spot tracking inaccuracies.
-
-    Args:
-        speeds (np.array): one player/ball speed (vx, vy) per row
-        acc (np.array): one player/ball acceleration (ax, ay) per row, same shape as speeds
-        max_speed (float): maximum speed allowed for a player or the ball
-
-    Returns:
-        acc: same as acc with value set to np.NaN if criterion <= 0
-    """
-    assert np is not None
-    criterion = -(9.1 / max_speed) * speeds + 9.1 - acc
-    mask = np.isnan(criterion)
-    criterion[mask] = -np.inf
-    mask_criterion = criterion <= 0.0
-    acc[mask_criterion] = np.nan
-    return acc
-
-
-def _apply_savgol_filter(raw_maps, polyorder, window_length):
-    """Smooth player/ball positions using a Savitzky-Golay filter.
-
-    Args:
-        raw_maps (np.array): one player/ball position (x, y) per row
-
-    Returns:
-        tracked_maps: smoothed player positions
-    """
-    if savgol_filter is None:
-        raise ImportError(
-            "Scipy is required to apply a Savitzky-Golay smoothing filter. "
-            "Please install it using: pip install scipy"
-        )
-
-    window_length = min(raw_maps.shape[0], window_length)
-    if window_length % 2 == 0:
-        window_length = window_length - 1
-    polyorder = min(window_length - 1, 1)
-    tracked_maps = savgol_filter(raw_maps, window_length, polyorder, axis=0)
-    return tracked_maps
 
 
 __all__ = ["Frame", "TrackingDataset", "Detection", "Trajectory"]
--- kloppy/tests/test_tracking.py
+++ kloppy/tests/test_tracking.py
@@ -1,28 +1,37 @@
-from math import sqrt, isnan
-import numpy as np
+from math import isnan, sqrt
 
+import numpy as np
 import pytest
 
 from kloppy.domain import (
-    Period,
     DatasetFlag,
-    Point,
-    TrackingDataset,
-    NormalizedPitchDimensions,
-    Dimension,
-    Orientation,
-    Frame,
-    Metadata,
-    Team,
-    Ground,
-    Player,
     Detection,
+    Dimension,
+    Frame,
+    Ground,
+    Metadata,
+    NormalizedPitchDimensions,
+    Orientation,
+    Period,
+    Player,
+    Point,
     Point3D,
+    Team,
+    TrackingDataset,
 )
 
 
 class TestTrackingDataset:
     def _get_tracking_dataset(self):
+        """Create a dummy tracking dataset.
+
+        The dataset contains a single player moving in a straight line with
+        the ball on their feet. During the first second (or 25 frames), the
+        player does not move. During the next four seconds (or 100 frames),
+        the player moves at a constant speed.
+
+        No player was detected in frame 10.
+        """
         home_team = Team(team_id="home", name="home", ground=Ground.HOME)
         home_team.players = [
             Player(team=home_team, player_id=f"home_1", jersey_no=1)
@@ -76,7 +85,41 @@ class TestTrackingDataset:
                     },
                     other_data={"extra_data": 1},
                 )
-                for i in range(25)  # not moving for one second
+                for i in range(10)  # not moving for one second
+            ]
+            + [
+                Frame(
+                    frame_id=10,
+                    timestamp=10 / 25,
+                    ball_owning_team=teams[0],
+                    ball_state=None,
+                    period=periods[0],
+                    objects={
+                        "ball": Detection(
+                            coordinates=Point3D(x=0, y=0, z=0),
+                        )
+                    },
+                    other_data={"extra_data": 1},
+                )
+            ]
+            + [
+                Frame(
+                    frame_id=i,
+                    timestamp=i / 25,
+                    ball_owning_team=teams[0],
+                    ball_state=None,
+                    period=periods[0],
+                    objects={
+                        "ball": Detection(
+                            coordinates=Point3D(x=0, y=0, z=0),
+                        ),
+                        home_team.players[0]: Detection(
+                            coordinates=Point(x=0, y=0),
+                        ),
+                    },
+                    other_data={"extra_data": 1},
+                )
+                for i in range(14)  # not moving for one second
             ]
             + [
                 Frame(
@@ -100,64 +143,59 @@ class TestTrackingDataset:
         )
         return tracking_data
 
-    def test_compute_kinematics(self):
-        dataset = (
-            self._get_tracking_dataset()
-            .compute_ball_kinematics(
-                # disable all filters
-                n_smooth_speed=1,
-                n_smooth_acc=1,
-                filter_type=None,
-                copy=True,
-            )
-            .compute_player_kinematics(
-                # disable all filters
-                n_smooth_speed=1,
-                n_smooth_acc=1,
-                filter_type=None,
-                copy=False,
-            )
-        )
-
-        ball_speeds = [frame.ball_data.speed for frame in dataset.records]
-        # The ball speed is not defined for the first frame
-        assert isnan(ball_speeds[0])
-        # The ball ball does not move for the first 25 frames
-        assert ball_speeds[1:25] == pytest.approx([0] * 24)
-        # The ball moves with a constant speed in the next 100 frames
-        assert ball_speeds[26:] == pytest.approx(
-            [sqrt(105**2 + 68**2) / 4] * 99
-        )
-
-        ball_accelerations = [
-            frame.ball_data.acceleration for frame in dataset.records
-        ]
-        # The ball acceleration is not defined for the first frame
-        assert isnan(ball_accelerations[0])
-        # The ball only accelerates at the 26th frame
-        assert ball_accelerations[25] == pytest.approx(0)
-        assert np.nanargmax(ball_accelerations) == 26
-        assert ball_accelerations[27] == pytest.approx(0)
-
-        # It should also compute the speed and acceleration for the players
-        for player in dataset.metadata.teams[0].players:
-            player_speeds = [
-                frame.players_data[player].speed for frame in dataset.records
-            ]
-            assert player_speeds[26:] == pytest.approx(
-                [sqrt(105**2 + 68**2) / 4] * 99
-            )
-            player_accelerations = [
-                frame.players_data[player].acceleration
-                for frame in dataset.records
-            ]
-            assert np.nanargmax(player_accelerations) == 26
-
-    def test_compute_kinematics_with_filter(self):
+    def test_ball_data(self):
         dataset = self._get_tracking_dataset()
-        dataset.compute_ball_kinematics(
-            n_smooth_speed=2, n_smooth_acc=2, filter_type="savitzky_golay"
+        frame = dataset[0]
+        assert frame.ball_data == Detection(
+            coordinates=Point3D(x=0, y=0, z=0),
         )
-        dataset.compute_ball_kinematics(
-            n_smooth_speed=2, n_smooth_acc=2, filter_type="moving_average"
+        assert frame.ball_coordinates == Point3D(x=0, y=0, z=0)
+        assert frame.ball_speed is None
+
+    def test_players_data(self):
+        dataset = self._get_tracking_dataset()
+        frame = dataset[0]
+        assert frame.players_data == {
+            dataset.metadata.teams[0].players[0]: Detection(
+                coordinates=Point(x=0, y=0),
+            )
+        }
+        assert frame.players_coordinates == {
+            dataset.metadata.teams[0].players[0]: Point(x=0, y=0),
+        }
+
+    def test_getitem(self):
+        dataset = self._get_tracking_dataset()
+        frame = dataset[0]
+
+        assert frame["ball"] == Detection(
+            coordinates=Point3D(x=0, y=0, z=0),
+        )
+        assert frame["home_1"] == Detection(
+            coordinates=Point(x=0, y=0),
+        )
+        assert frame[dataset.metadata.teams[0].players[0]] == Detection(
+            coordinates=Point(x=0, y=0),
+        )
+        assert frame["home_2"] is None
+
+    def test_trajectories(self):
+        dataset = self._get_tracking_dataset()
+
+        ball_trajectories = dataset.trajectories("ball")
+        assert len(ball_trajectories) == 1
+        assert ball_trajectories[0].start_frame == 0
+        assert ball_trajectories[0].end_frame == 124
+        assert len(ball_trajectories[0].detections) == 125
+        assert ball_trajectories[0].detections[0] == Detection(
+            coordinates=Point3D(x=0, y=0, z=0)
+        )
+
+        player_trajectories = dataset.trajectories("home_1")
+        assert len(player_trajectories) == 2
+        assert player_trajectories[0].start_frame == 0
+        assert player_trajectories[0].end_frame == 9
+        assert len(player_trajectories[0].detections) == 10
+        assert player_trajectories[0].detections[0] == Detection(
+            coordinates=Point(x=0, y=0)
         )

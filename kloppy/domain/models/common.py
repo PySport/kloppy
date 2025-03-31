2bbb2f22f185ab54a0672c624953a225cae59ed1
UnravelSports <64530306+UnravelSports@users.noreply.github.com>
2025-03-31 08:54:59 +0200

    woops

--- kloppy/domain/models/common.py
+++ kloppy/domain/models/common.py
@@ -112,7 +112,6 @@ class Provider(Enum):
     STATSPERFORM = "statsperform"
     HAWKEYE = "hawkeye"
     SPORTVU = "sportvu"
-    RESPOVISION = "respovision"
     OTHER = "other"
 
     def __str__(self):
@@ -867,43 +866,6 @@ class SkillCornerCoordinateSystem(ProviderCoordinateSystem):
             )
 
 
-class RespoVisionCoordinateSystem(ProviderCoordinateSystem):
-    @property
-    def provider(self) -> Provider:
-        return Provider.RESPOVISION
-
-    @property
-    def origin(self) -> Origin:
-        return Origin.CENTER
-
-    @property
-    def vertical_orientation(self) -> VerticalOrientation:
-        return VerticalOrientation.BOTTOM_TO_TOP
-
-    @property
-    def pitch_dimensions(self) -> PitchDimensions:
-        if self._pitch_length is not None and self._pitch_width is not None:
-            return MetricPitchDimensions(
-                x_dim=Dimension(
-                    -1 * self._pitch_length / 2, self._pitch_length / 2
-                ),
-                y_dim=Dimension(
-                    -1 * self._pitch_width / 2, self._pitch_width / 2
-                ),
-                pitch_length=self._pitch_length,
-                pitch_width=self._pitch_width,
-                standardized=False,
-            )
-        else:
-            return MetricPitchDimensions(
-                x_dim=Dimension(None, None),
-                y_dim=Dimension(None, None),
-                pitch_length=None,
-                pitch_width=None,
-                standardized=False,
-            )
-
-
 class DatafactoryCoordinateSystem(ProviderCoordinateSystem):
     @property
     def provider(self) -> Provider:
@@ -1049,7 +1011,6 @@ def build_coordinate_system(
         Provider.DATAFACTORY: DatafactoryCoordinateSystem,
         Provider.SECONDSPECTRUM: SecondSpectrumCoordinateSystem,
         Provider.HAWKEYE: HawkEyeCoordinateSystem,
-        Provider.RESPOVISION: RespoVisionCoordinateSystem,
         Provider.SPORTVU: SportVUCoordinateSystem,
     }
 

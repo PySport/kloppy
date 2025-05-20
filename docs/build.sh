bd142060ad96bee2d44369485ae02d4a9e67166b
Pieter Robberechts <pieter.robberechts@kuleuven.be>
2025-05-20 12:08:46 +0200

    Fix build script

--- docs/build.sh
+++ docs/build.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Install D2
-curl -fsSL https://d2lang.com/install.sh | sh -s -- --prefix ./bin "$D2_VERSION"
-export PATH="$PWD/bin:$PATH"
+curl -fsSL https://d2lang.com/install.sh | sh -s -- --prefix $PWD/deps/d2
+export PATH="$PWD/deps/d2/bin:$PATH"
 
 # Install kloppy and docs requirements
 pip install .

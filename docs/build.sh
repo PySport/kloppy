ff4b2e04c2c13e71719a9ad2fa7155580421401a
Pieter Robberechts <pieter.robberechts@kuleuven.be>
2025-05-20 11:40:34 +0200

    use build script

--- docs/build.sh
+++ docs/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
-# Build MkDocs site
+# Install D2
+curl -fsSL https://d2lang.com/install.sh | sh -s -- -b ./bin "$D2_VERSION"
+export PATH="$PWD/bin:$PATH"
+
+# Install kloppy and docs requirements
+pip install .
 pip install -r docs-requirements.txt
+
+# Build MkDocs site
 mkdocs build -d site

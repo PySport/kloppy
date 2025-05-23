917cb98fdf0d70f8529c46cc5ddcf5240f647d66
Pieter Robberechts <pieter.robberechts@kuleuven.be>
2025-05-23 13:53:19 +0200

    documentation + minor tweaks

--- kloppy/infra/serializers/tracking/sportec/deserializer.py
+++ kloppy/infra/serializers/tracking/sportec/deserializer.py
@@ -2,7 +2,7 @@ import logging
 import warnings
 from collections import defaultdict
 from datetime import datetime, timedelta
-from typing import IO, Dict, NamedTuple, Optional, Union
+from typing import IO, Dict, NamedTuple, Optional, Set, Union
 
 from lxml import etree, objectify
 
@@ -29,6 +29,9 @@ from ..deserializer import TrackingDataDeserializer
 
 logger = logging.getLogger(__name__)
 
+BALL_STATUS = "BallStatus"
+BALL_POSSESSION = "BallPossession"
+
 GAME_SECTION_TO_PERIOD_ID = {
     "firstHalf": 1,
     "secondHalf": 2,
@@ -40,45 +43,60 @@ GAME_SECTION_TO_PERIOD_ID = {
 def _unstack_framesets(
     raw_data: IO[bytes],
     limit: Optional[int] = None,
-    only_alive: Optional[bool] = True,
+    only_alive: bool = True,
+    objects_to_skip: Optional[Set] = None,
 ) -> Dict[int, Dict[int, Dict]]:
-    """
-    Read all data and unstack framesets.
+    """Unstack framesets.
 
-    If a limit is given, will only parse the first `limit` frames
-    for each object.
+    Sportec groups frames per period and object in a frameset. This function
+    unstacks the framesets and returns a dictionary with the following format:
 
-    Output format:
-    {
-        10_000: {
-            'ball': {
-                'N': "10000",
-                'X': 20.92,
-                'Y': 2.84,
-                'Z': 0.08,
-                'S': 4.91,
-                'BallPossession': "2",
-                'BallStatus': "1"
-                [...]
+        {
+            10_000: {
+                'ball': {
+                    'N': "10000",
+                    'X': 20.92,
+                    'Y': 2.84,
+                    'Z': 0.08,
+                    'S': 4.91,
+                    'BallPossession': "2",
+                    'BallStatus': "1"
+                    [...]
+                },
+                'DFL-OBJ-002G3I': {
+                    'N': "10000",
+                    'X': "0.35",
+                    'Y': "-25.26",
+                    'S': "0.00",
+                    [...]
+                },
+                [....]
             },
-            'DFL-OBJ-002G3I': {
-                'N': "10000",
-                'X': "0.35",
-                'Y': "-25.26",
-                'S': "0.00",
-                [...]
-            },
-            [....]
-        },
-        10_001: {
-          ...
+            10_001: {
+              ...
+            }
         }
-    }
-    """
-    if not limit:
-        limit = float("inf")
 
-    raw_frames = defaultdict(lambda: defaultdict(dict))
+    The keys are the frame IDs, and the values are dictionaries with the object
+    IDs as keys and their attributes as values.
+
+    Args:
+        raw_data: The raw data stream to be parsed.
+        limit: If a limit is provided, the function will stop processing frames
+            for an object once the limit is reached.
+        only_alive: If True, only frames with a ball status of 1 will be
+            processed.
+        objects_to_skip: A set of object IDs to skip during processing.
+
+    Returns:
+        A dictionary with the unstacked framesets.
+
+    Notes:
+        This function assumes that the framesets are ordered by period and
+        that the frames in each frameset are ordered.
+    """
+    objects_to_skip = objects_to_skip or set()
+    frames = defaultdict(lambda: defaultdict(dict))
     frames_per_obj = defaultdict(int)
     current_period = None
     current_obj = None
@@ -92,12 +110,18 @@ def _unstack_framesets(
             if event == "start" and elem.tag == "FrameSet":
                 game_section = elem.get("GameSection")
                 if game_section in GAME_SECTION_TO_PERIOD_ID:
-                    # Do we still need to process this game section?
+                    # we can stop parsing if ...
                     if (
+                        # it is not a frameset of the first period
                         current_period is not None
+                        # we've finished parsing the current period
                         and current_period
                         != GAME_SECTION_TO_PERIOD_ID[game_section]
-                        and all(n > limit for n in frames_per_obj.values())
+                        # all objects have reached the limit
+                        and (
+                            limit
+                            and all(n > limit for n in frames_per_obj.values())
+                        )
                     ):
                         break
 
@@ -107,19 +131,30 @@ def _unstack_framesets(
                         if elem.get("TeamId") == "BALL"
                         else elem.get("PersonId")
                     )
+
+                    if current_obj in objects_to_skip:
+                        current_obj = (
+                            None  # Mark as invalid to skip Frame processing
+                        )
+
             elif event == "start" and elem.tag == "Frame":
+                # we can skip this frame if ...
                 if (
-                    current_period
-                    and current_obj
-                    and (int(elem.get("BallStatus", 0)) == 1 or not only_alive)
-                    and frames_per_obj.get(current_obj, 0) < limit
+                    # it does not track a known object in a known period
+                    (current_period and current_obj)
+                    # we are not tracking frames in which the ball is not in play
+                    and (not only_alive or int(elem.get(BALL_STATUS, 0)) == 1)
+                    # we have reached the limit for this object
+                    and (
+                        not limit or frames_per_obj.get(current_obj, 0) < limit
+                    )
                 ):
-                    frames_per_obj[current_obj] += 1
                     frame_id = int(elem.get("N"))
-                    raw_frames[current_period][frame_id][current_obj] = {
+                    frames[current_period][frame_id][current_obj] = {
                         k: v for k, v in elem.items()
                     }
-                elem.clear()
+                    frames_per_obj[current_obj] += 1
+
             elif event == "end" and elem.tag == "FrameSet":
                 elem.clear()
         finally:
@@ -127,7 +162,7 @@ def _unstack_framesets(
             while elem.getprevious() is not None:
                 del elem.getparent()[0]
 
-    return dict(raw_frames)
+    return dict(frames)
 
 
 class SportecTrackingDataInputs(NamedTuple):
@@ -145,7 +180,7 @@ class SportecTrackingDataDeserializer(TrackingDataDeserializer):
         limit: Optional[int] = None,
         sample_rate: Optional[float] = None,
         coordinate_system: Optional[Union[str, Provider]] = None,
-        only_alive: Optional[bool] = True,
+        only_alive: bool = True,
     ):
         super().__init__(limit, sample_rate, coordinate_system)
         self.only_alive = only_alive
@@ -156,27 +191,13 @@ class SportecTrackingDataDeserializer(TrackingDataDeserializer):
         with performance_logging("parse metadata", logger=logger):
             match_root = objectify.fromstring(inputs.meta_data.read())
             sportec_metadata = sportec_metadata_from_xml_elm(match_root)
-            teams = home_team, away_team = sportec_metadata.teams
             date = datetime.fromisoformat(
                 match_root.MatchInformation.General.attrib["KickoffTime"]
             )
             game_week = match_root.MatchInformation.General.attrib["MatchDay"]
             game_id = match_root.MatchInformation.General.attrib["MatchId"]
-
             periods = sportec_metadata.periods
-            transformer = self.get_transformer(
-                pitch_length=sportec_metadata.x_max,
-                pitch_width=sportec_metadata.y_max,
-            )
-
-        # Stream and process tracking data
-        with performance_logging("parse tracking data", logger=logger):
-            limit = self.limit
-            if self.sample_rate:
-                limit = int(limit / self.sample_rate) if limit else None
-            period_frames = _unstack_framesets(
-                inputs.raw_data, limit, self.only_alive
-            )
+            teams = home_team, away_team = sportec_metadata.teams
             player_map = {
                 player.player_id: player
                 for team in teams
@@ -187,16 +208,34 @@ class SportecTrackingDataDeserializer(TrackingDataDeserializer):
                 for official in (sportec_metadata.officials or [])
             }
 
+            transformer = self.get_transformer(
+                pitch_length=sportec_metadata.x_max,
+                pitch_width=sportec_metadata.y_max,
+            )
+
+        # Stream and process tracking data
+        with performance_logging("parse tracking data", logger=logger):
+            period_frames = _unstack_framesets(
+                inputs.raw_data,
+                limit=int(self.limit / self.sample_rate)
+                if self.sample_rate and self.limit
+                else self.limit,
+                only_alive=self.only_alive,
+                objects_to_skip=official_ids,
+            )
+
             frames = []
             sample = 1.0 / self.sample_rate if self.sample_rate else 1
             frame_count = 0
 
             for period in periods:
-                period_id = period.id
-                raw_period_frames = period_frames.get(period_id, {})
+                raw_period_frames = period_frames.get(period.id, {})
 
                 sorted_frame_ids = sorted(raw_period_frames.keys())
-                for i, frame_id in enumerate(sorted_frame_ids):
+                for (
+                    i,
+                    frame_id,
+                ) in enumerate(sorted_frame_ids):
                     if self.limit and frame_count >= self.limit:
                         break
 
@@ -204,8 +243,11 @@ class SportecTrackingDataDeserializer(TrackingDataDeserializer):
                     if "ball" not in frame_data:
                         continue
 
-                    ball_data = frame_data["ball"]
-                    if self.only_alive and ball_data.get("BallStatus") != "1":
+                    ball_data = frame_data.get("ball", {})
+                    if (
+                        self.only_alive
+                        and int(ball_data.get(BALL_STATUS, 0)) != 1
+                    ):
                         continue
 
                     if i % sample != 0:
@@ -223,24 +265,24 @@ class SportecTrackingDataDeserializer(TrackingDataDeserializer):
                                 / sportec_metadata.fps
                             ),
                             ball_owning_team=home_team
-                            if ball_data.get("BallPossession") == "1"
+                            if int(ball_data.get(BALL_POSSESSION, 0)) == 1
                             else away_team,
                             ball_state=BallState.ALIVE
-                            if ball_data.get("BallStatus") == "1"
+                            if int(ball_data.get(BALL_STATUS, 0)) == 1
                             else BallState.DEAD,
                             period=period,
                             players_data={
-                                player_map[player_id]: PlayerData(
+                                player_map[object_id]: PlayerData(
                                     coordinates=Point(
                                         x=float(data.get("X", 0)),
                                         y=float(data.get("Y", 0)),
                                     ),
                                     speed=float(data.get("S", 0)),
                                 )
-                                for player_id, data in frame_data.items()
-                                if player_id != "ball"
-                                and player_id not in official_ids
-                                and player_id in player_map
+                                for object_id, data in frame_data.items()
+                                if object_id != "ball"
+                                and object_id not in official_ids
+                                and object_id in player_map
                             },
                             ball_coordinates=Point3D(
                                 x=float(ball_data.get("X", 0)),
@@ -270,7 +312,7 @@ class SportecTrackingDataDeserializer(TrackingDataDeserializer):
             )
         except StopIteration:
             orientation = Orientation.NOT_SET
-            warnings.warn("Could not determine dataset orientation")
+            logger.warning("Could not determine dataset orientation")
 
         metadata = Metadata(
             teams=teams,

45a7b8c334c3324c24d45b201ca78f3a6a595c9a
Pieter Robberechts <pieter.robberechts@kuleuven.be>
2025-05-23 23:53:58 +0200

    fix docs/build.sh

--- docs/build.sh
+++ docs/build.sh
@@ -3,17 +3,17 @@ set -e
 
 # Set up local tools dir
 mkdir -p .tools/bin
-export PATH="$PWD/tools/bin:$PATH"
+export PATH="$PWD/.tools/bin:$PATH"
 
 # Install D2 (for diagrams)
-# if ! command -v d2 &>/dev/null; then
-curl -fsSL https://d2lang.com/install.sh | sh -s -- --prefix $PWD/.tools
-# fi
+if ! command -v d2 &>/dev/null; then
+	curl -fsSL https://d2lang.com/install.sh | sh -s -- --prefix $PWD/.tools
+fi
 
 # Install uv
-# if ! command -v uv &>/dev/null; then
-curl -LsSf https://astral.sh/uv/install.sh | env UV_UNMANAGED_INSTALL="$PWD/.tools/bin" sh
-# fi
+if ! command -v uv &>/dev/null; then
+	curl -LsSf https://astral.sh/uv/install.sh | env UV_UNMANAGED_INSTALL="$PWD/.tools/bin" sh
+fi
 
 # Create and activate virtual environment
 if [ ! -d ".venv" ]; then

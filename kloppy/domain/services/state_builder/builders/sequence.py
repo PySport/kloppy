00187cdd3e3ef6b5c9ddc008a4e2bb54f9fc54c9
lode-mgp <lode@mygameplan.ai>
2025-06-25 13:01:59 +0200

    handle duels at end of sequence

--- kloppy/domain/services/state_builder/builders/sequence.py
+++ kloppy/domain/services/state_builder/builders/sequence.py
@@ -18,6 +18,8 @@ from kloppy.domain import (
     InterceptionEvent,
     InterceptionResult,
     GoalkeeperQualifier,
+    DuelEvent,
+    DuelResult,
 )
 from ..builder import StateBuilder
 
@@ -31,20 +33,38 @@ class Sequence:
 CLOSE_SEQUENCE = (BallOutEvent, FoulCommittedEvent, ShotEvent)
 
 
-def should_open_sequence(
-    event: Event, state: Optional[Sequence] = None
-) -> bool:
-    open_sequence = False
+def is_possessing_event(event: Event) -> bool:
     if isinstance(event, (PassEvent, CarryEvent, RecoveryEvent, TakeOnEvent)):
-        open_sequence = True
-    if isinstance(event, GoalkeeperEvent):
-        open_sequence = event.get_qualifier_value(GoalkeeperQualifier) in [
-            GoalkeeperActionType.PICK_UP,
-            GoalkeeperActionType.CLAIM,
-        ]
-    if isinstance(event, InterceptionEvent):
-        open_sequence = event.result == InterceptionResult.SUCCESS
-    return open_sequence and (
+        return True
+    elif isinstance(event, GoalkeeperEvent) and event.get_qualifier_value(
+        GoalkeeperQualifier
+    ) in [
+        GoalkeeperActionType.PICK_UP,
+        GoalkeeperActionType.CLAIM,
+    ]:
+        return True
+    elif (
+        isinstance(event, InterceptionEvent)
+        and event.result == InterceptionResult.SUCCESS
+    ):
+        return True
+    else:
+        return False
+
+
+def should_open_sequence(
+    event: Event, next_event: Event, state: Optional[Sequence] = None
+) -> bool:
+    can_open_sequence = False
+    if is_possessing_event(event):
+        can_open_sequence = True
+    elif (
+        isinstance(event, DuelEvent)
+        and event.result == DuelResult.WON
+        and is_possessing_event(next_event)
+    ):
+        can_open_sequence = True
+    return can_open_sequence and (
         state is None
         or state.team != event.team
         or event.get_qualifier_value(SetPieceQualifier)
@@ -60,12 +80,12 @@ def should_close_sequence(event: Event) -> bool:
 class SequenceStateBuilder(StateBuilder):
     def initial_state(self, dataset: EventDataset) -> Sequence:
         for event in dataset.events:
-            if should_open_sequence(event):
+            if should_open_sequence(event, event.next_record):
                 return Sequence(sequence_id=0, team=event.team)
         return Sequence(sequence_id=0, team=None)
 
     def reduce_before(self, state: Sequence, event: Event) -> Sequence:
-        if should_open_sequence(event, state):
+        if should_open_sequence(event, event.next_record, state):
             state = replace(
                 state, sequence_id=state.sequence_id + 1, team=event.team
             )

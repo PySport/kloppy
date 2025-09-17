159bec300d02bcae3b4297206b17fb6d4c6d71d4
stephTchembeu <stephane.tchembeu@aims-cameroon.org>
2025-09-17 18:47:11 +0100

    definig the CDF serilizer body

--- kloppy/infra/serializers/tracking/cdf/serializer.py
+++ kloppy/infra/serializers/tracking/cdf/serializer.py
@@ -28,278 +28,220 @@ class CDFTrackingDataSerializer(TrackingDataSerializer[CDFOutputs]):
                   do a transformation if not in the right format?
         """
 
-        outputs.meta_data.write(
-            b"""{
-    "competition": {
-      "id": "comp_123",
-      "name": "Dutch Eredivisie Under 20",
-      "format": "league_20",
-      "age_restriction": null,
-      "type": "mens"
-    },
-    "season": {
-      "id": "season_2023",
-      "name": "2022/23"
-    },
-    "match": {
-      "id": "match_456",
-      "kickoff_time": "2023-05-15T19:45:00Z",
-      "periods": [
-        {
-          "period": "first_half",
-          "play_direction": "left_right",
-          "start_time": "2023-05-15T19:45:00Z",
-          "end_time": "2023-05-15T20:30:00Z",
-          "start_frame_id": 0,
-          "end_frame_id": 27000,
-          "left_team_id": "team_789",
-          "right_team_id": "team_101"
-        },
-        {
-          "period": "second_half",
-          "play_direction": "right_left",
-          "start_time": "2023-05-15T20:45:00Z",
-          "end_time": "2023-05-15T21:30:00Z",
-          "start_frame_id": 27001,
-          "end_frame_id": 54000,
-          "left_team_id": "team_101",
-          "right_team_id": "team_789"
-        }
-      ],
-      "whistles": [
-        {
-          "type": "first_half",
-          "sub_type": "start",
-          "time": "2023-05-15T19:45:00Z"
-        },
-        {
-          "type": "first_half",
-          "sub_type": "end",
-          "time": "2023-05-15T20:30:00Z"
-        },
-        {
-          "type": "second_half",
-          "sub_type": "start",
-          "time": "2023-05-15T20:45:00Z"
-        },
-        {
-          "type": "second_half",
-          "sub_type": "end",
-          "time": "2023-05-15T21:30:00Z"
-        }
-      ],
-      "round": "38",
-      "scheduled_kickoff_time": "2023-05-15T19:45:00Z",
-      "local_kickoff_time": "2023-05-15T20:45:00+01:00",
-      "misc": {
-        "country": "Netherlands",
-        "city": "Breda",
-        "percipitation": 0.5,
-        "is_open_roof": true
-      }
-    },
-    "teams": {
-      "home": {
-        "id": "team_789",
-        "players": [
-          {
-            "id": "player_1",
-            "team_id": "team_789",
-            "jersey_number": 1,
-            "is_starter": true
-          },
-          {
-            "id": "player_2",
-            "team_id": "team_789",
-            "jersey_number": 2,
-            "is_starter": true
-          },
-          {
-            "id": "player_3",
-            "team_id": "team_789",
-            "jersey_number": 3,
-            "is_starter": true
-          },
-          {
-            "id": "player_4",
-            "team_id": "team_789",
-            "jersey_number": 4,
-            "is_starter": true
-          },
-          {
-            "id": "player_5",
-            "team_id": "team_789",
-            "jersey_number": 5,
-            "is_starter": true
-          },
-          {
-            "id": "player_6",
-            "team_id": "team_789",
-            "jersey_number": 6,
-            "is_starter": true
-          },
-          {
-            "id": "player_7",
-            "team_id": "team_789",
-            "jersey_number": 7,
-            "is_starter": true
-          },
-          {
-            "id": "player_8",
-            "team_id": "team_789",
-            "jersey_number": 8,
-            "is_starter": true
-          },
-          {
-            "id": "player_9",
-            "team_id": "team_789",
-            "jersey_number": 9,
-            "is_starter": true
-          },
-          {
-            "id": "player_10",
-            "team_id": "team_789",
-            "jersey_number": 10,
-            "is_starter": true
-          },
-          {
-            "id": "player_11",
-            "team_id": "team_789",
-            "jersey_number": 11,
-            "is_starter": true
-          },
-          {
-            "id": "player_12",
-            "team_id": "team_789",
-            "jersey_number": 12,
-            "is_starter": false
-          }
-        ]
-      },
-      "away": {
-        "id": "team_101",
-        "players": [
-          {
-            "id": "player_101",
-            "team_id": "team_101",
-            "jersey_number": 1,
-            "is_starter": true
-          },
-          {
-            "id": "player_102",
-            "team_id": "team_101",
-            "jersey_number": 2,
-            "is_starter": true
-          },
-          {
-            "id": "player_103",
-            "team_id": "team_101",
-            "jersey_number": 3,
-            "is_starter": true
-          },
-          {
-            "id": "player_104",
-            "team_id": "team_101",
-            "jersey_number": 4,
-            "is_starter": true
-          },
-          {
-            "id": "player_105",
-            "team_id": "team_101",
-            "jersey_number": 5,
-            "is_starter": true
-          },
-          {
-            "id": "player_106",
-            "team_id": "team_101",
-            "jersey_number": 6,
-            "is_starter": true
-          },
-          {
-            "id": "player_107",
-            "team_id": "team_101",
-            "jersey_number": 7,
-            "is_starter": true
-          },
-          {
-            "id": "player_108",
-            "team_id": "team_101",
-            "jersey_number": 8,
-            "is_starter": true
-          },
-          {
-            "id": "player_109",
-            "team_id": "team_101",
-            "jersey_number": 9,
-            "is_starter": true
-          },
-          {
-            "id": "player_110",
-            "team_id": "team_101",
-            "jersey_number": 10,
-            "is_starter": true
-          },
-          {
-            "id": "player_111",
-            "team_id": "team_101",
-            "jersey_number": 11,
-            "is_starter": true
-          },
-          {
-            "id": "player_112",
-            "team_id": "team_101",
-            "jersey_number": 12,
-            "is_starter": false
-          }
-        ]
-      }
-    },
-    "stadium": {
-      "id": "stadium_202",
-      "pitch_length": 105.0,
-      "pitch_width": 68.0,
-      "name": "A Stadium",
-      "turf": "grass"
-    },
-    "meta": {
-      "video": {
-        "perspective": "stadium",
-        "version": "0.1.0",
-        "name": "VideoVendor",
-        "fps": 25
-      },
-      "tracking": {
-        "version": "0.1.0",
-        "name": "TrackingVendor",
-        "fps": 25,
-        "collection_timing": "live"
-      },
-      "landmarks": {
-        "version": "0.1.0",
-        "name": "LimbTrackingVendor",
-        "fps": 25,
-        "collection_timing": "post"
-      },
-      "ball": {
-        "version": "0.1.0",
-        "name": "BallTrackingVendor",
-        "fps": 50,
-        "collection_timing": "live"
-      },
-      "event": {
-        "collection_timing": "live"
-      },
-      "meta": {
-        "version": "0.1.0",
-        "name": "Meta Vendor"
-      },
-      "cdf": {
-        "version": "0.2.0"
-      }
-    }
-  }"""
+        # Normalize the coordinate system
+        # creating the coordinate system according to the CDF paper specifications.
+        from kloppy.domain import (
+            CustomCoordinateSystem,
+            Origin,
+            VerticalOrientation,
+            NormalizedPitchDimensions,
+            Dimension,
+            Orientation,
+            BallState
         )
-        outputs.tracking_data.write(
-            b'{"frame_id":123456, "timestamp": "2023-10-01T12:00:00Z","period":"first_half","match":{"id":"match_12345"},"ball_status": true,"teams":{"home":{"id":"team_1","players":[{"id":"player_1","x":23.5,"y":45.2},{"id":"player_2","x":56.3,"y":12.8}]},"away":{"id":"team_2","players":[{"id":"player_3","x":78.1,"y":34.9},{"id":"player_4","x":45.6,"y":89}]}},"ball":{"x":50,"y":50,"z":0.5}}'
+        # length and width of the pitch
+        length = dataset.metadata.pitch_dimensions.pitch_length
+        width = dataset.metadata.pitch_dimensions.pitch_length
+        CDF_coordinate_system = CustomCoordinateSystem(
+            origin=Origin.CENTER,
+            vertical_orientation=VerticalOrientation.BOTTOM_TO_TOP,
+            pitch_dimensions=NormalizedPitchDimensions(
+                x_dim=Dimension(min=-length / 2, max=length / 2),
+                y_dim=Dimension(min=-width / 2, max=width / 2),
+                pitch_length=length,
+                pitch_width=width,
+            ),
         )
+        # setting it as coordinate system of the imported data
+        dataset = dataset.transform(
+            to_coordinate_system=CDF_coordinate_system,
+            to_orientation=Orientation.STATIC_HOME_AWAY,
+        )
+        ##--------------------------------------------------------------------------
+
+        ## building Tracking jsonl
+        # Output containers
+        metadata_json = {}
+
+        # Convert the dataset into a DataFrame
+        periods = {
+            1: "first_half",
+            2: "second_half",
+            3: "first_half_extratime",
+            4: "second_half_extratime",
+            5: "shootout",
+        }
+
+        # Get home and away team data
+        home_team, away_team = dataset.metadata.teams
+        # Get the players Id.
+        home_player_ids, away_player_ids = (
+            [player.player_id for player in home_team.players],
+            [player.player_id for player in away_team.players],
+        )
+
+        for frame_id in range(len([1])):
+            frame_data = {}
+
+            # Frame ID
+            frame_data["frame_id"] = frame_id
+            # Timestamp
+            frame_data["timestamp"] = dataset[frame_id].timestamp
+            # Period
+            frame_data["period"] = periods.get(dataset[frame_id].period, "unknown")
+            # Match ID (placeholder)
+            frame_data["match"] = {"id": dataset.metadata.game_id}
+            # Ball status
+            ball_state = dataset[frame_id].ball_state
+            frame_data["ball_status"] = dataset[0].ball_state == BallState.ALIVE
+
+            # Teams and players
+            home_players = []
+            for player, coordinates in dataset[frame_id].players_coordinates.items():
+                if player.player_id in home_player_ids:
+                    try:
+                        x = coordinates.x
+                        y = coordinates.x
+                        home_players.append(
+                            {"id": player.player_id, "x": round(x, 3), "y": round(y, 3)}
+                        )
+                    except KeyError:
+                        continue
+
+            away_players = []
+            for player, coordinates in dataset[frame_id].players_coordinates.items():
+                if player.player_id in away_player_ids:
+                    try:
+                        x = coordinates.x
+                        y = coordinates.x
+                        home_players.append(
+                            {"id": player.player_id, "x": round(x, 3), "y": round(y, 3)}
+                        )
+                    except KeyError:
+                        continue
+
+            frame_data["teams"] = {
+                "home": {"id": home_team.team_id, "players": home_players},
+                "away": {"id": away_team.team_id, "players": away_players},
+            }
+
+            # Ball
+            if frame_data["ball_status"] == True:
+                try:
+                    ball_x = round(dataset[0].ball_coordinates.x, 3)
+                    ball_y = round(dataset[0].ball_coordinates.y, 3)
+                    ball_z = round(dataset[0].ball_coordinates.z, 3)
+                except KeyError:
+                    ball_x = ball_y = ball_z = None
+                frame_data["ball"] = {"x": ball_x, "y": ball_y, "z": ball_z}
+
+            # Add to tracking list
+            outputs.tracking_data.write(frame_data)
+
+        ### build now the metadata.
+
+        # Copetition infos.
+        metadata_json["competition"] = {  # w don't have any of these informations
+            "id": "",
+            "name": "",
+            "format": "",
+            "age_restriction": "null",
+            "type": "",
+        }
+
+        # season infos.
+        metadata_json["season"] = {  # w don't have any of these informations
+            "id": "",
+            "name": "",
+        }
+
+        # match infos.
+        periods_info = []
+        for period in dataset.metadata.periods:
+            curent_period = {
+                "period": periods[period.id],
+                "play_direction": "left_to_rigth",
+                "start_time": dataset.metadata.date + period.start_time.timestamp,
+                "end_time": dataset.metadata.date + period.end_time.timestamp,
+                "start_frame_id": (
+                    0
+                    if period.id == 1
+                    else len(dataset.filter(lambda frame: frame.period.id == 1).to_df())
+                ),
+                "end_frame_id": (
+                    len(dataset.filter(lambda frame: frame.period.id == period.id).to_df())
+                    - 1
+                    if period.id == 1
+                    else len(dataset.filter(lambda frame: frame.period.id == 1).to_df())
+                    + len(
+                        dataset.filter(lambda frame: frame.period.id == period.id).to_df()
+                    )
+                    - 1
+                ),
+                "left_team_id": home_team.team_id,
+                "right_team_id": away_team.team_id,
+            }
+            periods_info.append(curent_period)
+
+            ## building team_players for metadata
+        meta_home_players = []
+        starters_ids = []
+        for player, coordinates in dataset[0].players_coordinates.items():
+            starters_ids.append(player.player_id)
+
+        for player in home_team.players:
+            try:
+                home_players.append(
+                    {
+                        "id": player.player_id,
+                        "team_id": home_team.team_id,
+                        "jersey_number": player.jersey_no,
+                        "is_starter": player.player_id in starters_ids,
+                    }
+                )
+            except KeyError:
+                continue
+
+        meta_away_players = []
+        for player in away_team.players:
+            try:
+                away_players.append(
+                    {
+                        "id": player.player_id,
+                        "team_id": away_team.team_id,
+                        "jersey_number": player.jersey_no,
+                        "is_starter": player.player_id in starters_ids,
+                    }
+                )
+            except KeyError:
+                continue
+
+        metadata_json["match"] = {
+            "match": {
+                "id": dataset.metadata.game_id,  # same as for the jsonl
+                "kickoff_time": dataset.metadata.periods[0].start_time,
+                "periods": periods_info,
+                "round": "38",
+                "scheduled_kickoff_time": "",  # how to get this ?
+                "local_kickoff_time": "",  # how to get this ?
+                "misc": {
+                    "country": "",  # how to get this ?
+                    "city": "",  # how to get this ?
+                    "percipitation": 0.5,  # how to get this ?
+                    "is_open_roof": True,  # how to get this ?
+                },
+            },
+            "teams": {
+                "home": {
+                    "id": home_team.team_id,  # same as for the jsonl
+                    "players": meta_home_players,
+                },
+                "away": {
+                    "id": away_team.team_id,  # same as for the jsonl
+                    "players": meta_away_players,
+                },
+            },
+        }
+
+        outputs.meta_data.write(metadata_json)
         return True

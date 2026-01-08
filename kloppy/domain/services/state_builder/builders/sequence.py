628a05f6aa1b2314e14998b5043fa2e0081890c0
lode-mgp <lode@mygameplan.ai>
2026-01-08 15:06:31 +0100

    fix: Improve the sequence state builder (#484)
    * more events can start sequence
    * handle duels at end of sequence
    * exclude off ball events from sequences

--- kloppy/domain/services/state_builder/builders/sequence.py
+++ kloppy/domain/services/state_builder/builders/sequence.py
@@ -1,42 +1,113 @@
 from dataclasses import dataclass, replace
+from typing import Optional
 
 from kloppy.domain import (
     BallOutEvent,
+    CardEvent,
     CarryEvent,
+    ClearanceEvent,
+    DuelEvent,
+    DuelResult,
     Event,
     EventDataset,
+    FormationChangeEvent,
     FoulCommittedEvent,
+    GenericEvent,
+    GoalkeeperActionType,
+    GoalkeeperEvent,
+    GoalkeeperQualifier,
+    InterceptionEvent,
+    InterceptionResult,
     PassEvent,
+    PlayerOffEvent,
+    PlayerOnEvent,
     RecoveryEvent,
     SetPieceQualifier,
     ShotEvent,
+    SubstitutionEvent,
+    TakeOnEvent,
     Team,
 )
 
 from ..builder import StateBuilder
 
-OPEN_SEQUENCE = (PassEvent, CarryEvent, RecoveryEvent)
-CLOSE_SEQUENCE = (BallOutEvent, FoulCommittedEvent, ShotEvent)
-
 
 @dataclass
 class Sequence:
-    sequence_id: int
-    team: Team
+    sequence_id: Optional[int]
+    team: Optional[Team]
+
+
+EXCLUDED_OFF_BALL_EVENTS = (
+    GenericEvent,
+    SubstitutionEvent,
+    CardEvent,
+    PlayerOnEvent,
+    PlayerOffEvent,
+    FormationChangeEvent,
+)
+
+CLOSE_SEQUENCE = (BallOutEvent, FoulCommittedEvent, ShotEvent)
+
+
+def is_ball_winning_defensive_action(event: Event) -> bool:
+    if isinstance(event, DuelEvent) and event.result == DuelResult.WON:
+        return True
+    elif isinstance(event, ClearanceEvent):
+        return True
+
+
+def is_possessing_event(event: Event) -> bool:
+    if isinstance(event, (PassEvent, CarryEvent, RecoveryEvent, TakeOnEvent)):
+        return True
+    elif isinstance(event, GoalkeeperEvent) and event.get_qualifier_value(
+        GoalkeeperQualifier
+    ) in [
+        GoalkeeperActionType.PICK_UP,
+        GoalkeeperActionType.CLAIM,
+    ]:
+        return True
+    elif (
+        isinstance(event, InterceptionEvent)
+        and event.result == InterceptionResult.SUCCESS
+    ):
+        return True
+
+
+def should_open_sequence(
+    event: Event, next_event: Optional[Event], state: Optional[Sequence] = None
+) -> bool:
+    can_open_sequence = False
+    if is_possessing_event(event):
+        can_open_sequence = True
+    elif (
+        is_ball_winning_defensive_action(event)
+        and next_event is not None
+        and next_event.team == event.team
+        and is_possessing_event(next_event)
+    ):
+        can_open_sequence = True
+    return can_open_sequence and (
+        state is None
+        or state.team != event.team
+        or event.get_qualifier_value(SetPieceQualifier)
+    )
+
+
+def should_close_sequence(event: Event) -> bool:
+    if isinstance(event, CLOSE_SEQUENCE):
+        return True
 
 
 class SequenceStateBuilder(StateBuilder):
     def initial_state(self, dataset: EventDataset) -> Sequence:
         for event in dataset.events:
-            if isinstance(event, OPEN_SEQUENCE):
+            if should_open_sequence(event, event.next_record):
                 return Sequence(sequence_id=0, team=event.team)
         return Sequence(sequence_id=0, team=None)
 
     def reduce_before(self, state: Sequence, event: Event) -> Sequence:
-        if isinstance(event, OPEN_SEQUENCE) and (
-            state.team != event.team
-            or event.get_qualifier_value(SetPieceQualifier)
-        ):
+        if should_open_sequence(event, event.next_record, state):
             state = replace(
                 state, sequence_id=state.sequence_id + 1, team=event.team
             )
@@ -48,3 +119,28 @@ class SequenceStateBuilder(StateBuilder):
             state = replace(state, sequence_id=state.sequence_id + 1, team=None)
 
         return state
+
+    def post_process(self, events: list[Event]):
+        current_sequence_id = 1
+        sequence_id_mapping = {}
+
+        for event in events:
+            sequence = event.state["sequence"]
+
+            if (
+                isinstance(event, EXCLUDED_OFF_BALL_EVENTS)
+                or sequence.team is None
+            ):
+                event.state["sequence"] = Sequence(sequence_id=None, team=None)
+            elif sequence.sequence_id is not None:
+                # Map old sequence IDs to new consecutive IDs
+                # Get or assign a new sequence ID
+                new_sequence_id = sequence_id_mapping.setdefault(
+                    sequence.sequence_id, current_sequence_id
+                )
+                if new_sequence_id == current_sequence_id:
+                    current_sequence_id += 1
+                # Assign the new sequence ID
+                event.state["sequence"] = Sequence(
+                    sequence_id=new_sequence_id, team=sequence.team
+                )
